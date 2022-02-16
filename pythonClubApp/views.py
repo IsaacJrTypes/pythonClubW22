da68bc5eb637c4f8903fcdcdb9aa06e3e6db5a14
Isaac J <duckhunterjr@gmail.com>
2022-02-16 14:02:58 -0800

    add assig 6: meetings & details

--- pythonClubApp/views.py
+++ pythonClubApp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting, Resource
 # Create your views here.
 def index(request):
 	return render(request, 'pythonClubApp/index.html')
@@ -7,3 +7,12 @@ def index(request):
 def resources(request):
 	resourceList = Resource.objects.all()
 	return render(request, 'pythonClubApp/resources.html', {'resourceList': resourceList})
+
+def meetings(request):
+	meetingList=Meeting.objects.all()
+	return render(request,'pythonClubApp/meetings.html', {'meetingList': meetingList})
+
+def meetingDetails(request,id):
+	meeting=get_object_or_404(Meeting,pk=id)
+	return render(request,'pythonClubApp/meetingdetails.html', {'meeting': meeting})
+
